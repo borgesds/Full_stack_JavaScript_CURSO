bf527bc3fc29a38f32685e98f38dbdb131d88e38
borgesds <borges.an@hotmail.com>
2022-04-15 19:33:02 -0300

    up Pratica

---
+++ juncao_javascript_html/1_pratica_eventos_no_HTML/events.js
@@ -0,0 +1,19 @@
+function clickMe() {
+    console.log("Fui Clicado")
+}
+
+function focusMe() {
+    console.log("Fui focado")
+}
+
+function bluerMe() {
+    console.log("Fui desfocado")
+}
+
+function changeMe() {
+    console.log("Fui Alterado")
+}
+
+function overMe() {
+    console.log("Mouse aqui")
+}
---
+++ juncao_javascript_html/2_pratica_selecionandoElementosnoDOM/select_elements.js
@@ -0,0 +1,10 @@
+function show() {
+    //let element = document.getElementsByName("phone")
+    //console.log(element[0].value)
+
+    //let element = document.querySelectorAll("div#phones input[name=phone]")
+    //console.log(element[1].value)
+
+    let element = document.getElementsByTagName("input")
+    console.log(element)
+}
