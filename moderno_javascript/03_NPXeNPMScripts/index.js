767c384f8314a400788e94d05b8013362013dedd
borgesds <borges.an@hotmail.com>
2022-04-28 07:06:56 -0300

    up Pratica

---
+++ moderno_javascript/03_NPXeNPMScripts/index.js
@@ -0,0 +1,7 @@
+//Um atalho para poder converter os arquivos JS para versões anteriores:
+   //npx babel nomeDoArquivo.js -o nomeDoArquivoNovo.js
+
+//sudo npm install http-server --global (cria um servidor local)
+// iniciar o mini servidor:
+    //http-server
+console.log("Testando conversão")
