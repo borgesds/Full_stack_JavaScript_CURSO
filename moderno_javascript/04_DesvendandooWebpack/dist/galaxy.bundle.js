c0a868b73c4024eec661dca04eb3e3027164caa7
borgesds <borges.an@hotmail.com>
2022-05-03 18:52:04 -0300

    up Pratica

--- moderno_javascript/04_DesvendandooWebpack/dist/galaxy.bundle.js
+++ moderno_javascript/04_DesvendandooWebpack/dist/galaxy.bundle.js
@@ -15,7 +15,7 @@
   \***********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://04_desvendandoowebpack/./src/galaxy.js?");
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Unknown option: .preset. Check out https://babeljs.io/docs/en/babel-core/#options for more information about options.\\n    at throwUnknownError (/home/borges/Documentos/vscode/onebitcode/exercicios/moderno_javascript/04_DesvendandooWebpack/node_modules/@babel/core/lib/config/validation/options.js:133:27)\\n    at /home/borges/Documentos/vscode/onebitcode/exercicios/moderno_javascript/04_DesvendandooWebpack/node_modules/@babel/core/lib/config/validation/options.js:118:5\\n    at Array.forEach (<anonymous>)\\n    at validateNested (/home/borges/Documentos/vscode/onebitcode/exercicios/moderno_javascript/04_DesvendandooWebpack/node_modules/@babel/core/lib/config/validation/options.js:94:21)\\n    at validate (/home/borges/Documentos/vscode/onebitcode/exercicios/moderno_javascript/04_DesvendandooWebpack/node_modules/@babel/core/lib/config/validation/options.js:85:10)\\n    at /home/borges/Documentos/vscode/onebitcode/exercicios/moderno_javascript/04_DesvendandooWebpack/node_modules/@babel/core/lib/config/config-chain.js:204:34\\n    at cachedFunction (/home/borges/Documentos/vscode/onebitcode/exercicios/moderno_javascript/04_DesvendandooWebpack/node_modules/@babel/core/lib/config/caching.js:60:27)\\n    at cachedFunction.next (<anonymous>)\\n    at evaluateSync (/home/borges/Documentos/vscode/onebitcode/exercicios/moderno_javascript/04_DesvendandooWebpack/node_modules/gensync/index.js:251:28)\\n    at sync (/home/borges/Documentos/vscode/onebitcode/exercicios/moderno_javascript/04_DesvendandooWebpack/node_modules/gensync/index.js:89:14)\");\n\n//# sourceURL=webpack://04_desvendandoowebpack/./src/galaxy.js?");
 
 /***/ })
 
@@ -24,7 +24,7 @@ eval("\n\n//# sourceURL=webpack://04_desvendandoowebpack/./src/galaxy.js?");
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/galaxy.js"]();
 /******/ 	
--- moderno_javascript/04_DesvendandooWebpack/webpack.config.js
+++ moderno_javascript/04_DesvendandooWebpack/webpack.config.js
@@ -9,5 +9,21 @@ module.exports = {
     output: {
         filename: '[name].bundle.js', 
         path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [{
+            test: /\.js$/, //Pega todos os arquivos que tem .JS
+            use: ['babel-loader']
+        }]
+    },
+
+    watch: true,
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+            watch: true,
+        }
     }
 }
