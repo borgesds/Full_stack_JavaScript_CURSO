c4feaa117b327aabbf1388ae8a19f1a27c4c7511
borgesds <borges.an@hotmail.com>
2022-05-06 07:17:24 -0300

    up Pratica

---
+++ moderno_javascript/06_ARQUIVO_PADRAO/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        index: './src/index.js', 
+        galaxy: './src/galaxy.js'
+    },
+
+    output: {
+        filename: '[name].bundle.js', 
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [{
+            test: /\.js$/, //Pega todos os arquivos que tem .JS
+            use: ['babel-loader']
+        }]
+    },
+
+    watch: true,
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+            watch: true,
+        }
+    }
+}
