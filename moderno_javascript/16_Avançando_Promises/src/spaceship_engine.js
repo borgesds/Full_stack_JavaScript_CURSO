4e560672fc0a865768fe388cbbed6c83e72cf25a
borgesds <borges.an@hotmail.com>
2022-06-06 20:51:15 -0300

    up Async

--- moderno_javascript/16_Avançando_Promises/src/spaceship_engine.js
+++ moderno_javascript/16_Avançando_Promises/src/spaceship_engine.js
@@ -2,49 +2,45 @@ export default class {
   constructor(spaceship) {
     this.spaceship = spaceship
   }
-  turnOn() {
-    let currentChargeChecking = this.checkCurrentCharge()
-    let shieldChecking = this.testShield()
-    Promise.all([currentChargeChecking, shieldChecking]).then(results => {
-      return this.shildNormalize(results[1])
-    }).then(newShield => {
-      this.spaceship.shield = newShield
+
+  async turnOn() {
+    try{
+      let currentChargeChecking = this.checkCurrentCharge()
+      let shieldChecking = this.testShield()
+      let result = await Promise.all([currentChargeChecking, shieldChecking])
+      this.spaceship.shield = await this.shieldNormalize(results[1])
+      
       console.log(`(${this.spaceship.name} Partida não autorizada: ${this.spaceship.shield}) - Carga(${this.spaceship.currentCharge}GJ)`)
-    }).catch(error => {
+      
+    } catch (error) {
       console.log(`(${this.spaceship.name}) Partida não autorizada: ${error}`)
-    })
+    }
   }
 
-  checkCurrentCharge() {
-    return new Promise((resolve, reject) => {
-      let currentCharge = this.spaceship.currentPercentCharge()
-      if(currentCharge >= 30) {
-        resolve(currentCharge)
-      } else {
-        reject(`Carga em apenas ${currentCharge}GJ`)
-      }
-    })
+  async checkCurrentCharge() {
+    let currentCharge = this.spaceship.currentPercentCharge()
+    if(currentCharge < 30) {
+      return Promise.reject(`Carga em apenas ${currentCharge}GJ`)
+    }
+    return currentCharge
+
   }
 
-  testShield() {
-    return new Promise((resolve, reject) => {
-      let doubledShield = this.spaceship.shield * 2
-      if (doubledShield >= 100) {
-        resolve(doubledShield)
-      } else {
-        reject("Escudo em sobrecarga")
-      }
-    })
+  async testShield() {
+    let doubledShield = this.spaceship.shield * 2
+    if (doubledShield < 100) {
+      return Promise.reject("Escudo em sobrecarga")
+    }
+    return doubledShield
+    
   }
 
-  shildNormalize(shield) {
-    return new Promise((resolve, reject) => {
-      let normalizedShield = shield * 0.7
-      if(normalizedShield > 120) {
-        reject("Escudo em Supercarga")
-      } else {
-        resolve(normalizedShield)
-      }
-    })
+  async shieldNormalize(shield) {
+    let normalizedShield = shield * 0.7
+    if(normalizedShield > 120) {
+      return Promise.reject("Escudo em Supercarga")
+    }
+    return normalizedShield
+
   }
 }
