24564eb381a2d6cc167b4244da9e50834625dd30
borgesds <borges.an@hotmail.com>
2022-06-08 06:50:57 -0300

    up API nasa

--- moderno_javascript/19_Requisições_Get/src/index.js
+++ moderno_javascript/19_Requisições_Get/src/index.js
@@ -12,10 +12,20 @@ async function loadNeos() {
         const maxDiameter = neo["estimated_diameter"]["kilometers"]["estimated_diameter_max"]
         const averageDiameter = (minDiameter + maxDiameter) / 2
         const newNeo = new Neo(neo["id"], neo["name"], averageDiameter, neo["is_sentry_object"])
-
         neos.push(newNeo) 
     })
-    console.log(neos)
+    renderNEOs(neos)
 }
+
+function renderNEOs(neos) {
+    const ulElement = document.getElementById("neos-list")
+    neos.forEach(neo => {
+        const liElement = document.createElement("li")
+        const isSentry = neo.isSentry ? "Perigo de colisão" : "Sem perigo de colisão"
+        const text = `${neo.id} | ${neo.name} | ${neo.averageEstimatedDiameter} | ${isSentry}`
+        liElement;innerText = text
+        ulElement.appendChild(liElement)
+    })
+} 
 
 loadNeos()
