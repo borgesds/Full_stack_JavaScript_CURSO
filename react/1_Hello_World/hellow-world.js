33cd1603415633d04cd1202f469b0817e1b271a9
borgesds <borges.an@hotmail.com>
2022-06-09 19:09:15 -0300

    up React first

---
+++ react/1_Hello_World/hellow-world.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+    React.createElement(
+        'h1',
+        null,
+        'Hello world'
+
+    ),
+    document.getElementById('root')
+)
---
+++ react/2_Gerando_primeiro_projeto/my-first-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HelloWorld from './hellow';
+
+
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+
+    </div>
+  );
+}
+
+export default App;
