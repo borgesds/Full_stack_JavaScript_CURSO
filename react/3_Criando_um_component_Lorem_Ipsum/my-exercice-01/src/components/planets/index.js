fce9eafe3b78deef9b8d4765bd8288330db24b4e
borgesds <borges.an@hotmail.com>
2022-06-13 19:50:11 -0300

    up React get api

--- react/3_Criando_um_component_Lorem_Ipsum/my-exercice-01/src/components/planets/index.js
+++ react/3_Criando_um_component_Lorem_Ipsum/my-exercice-01/src/components/planets/index.js
@@ -1,27 +1,28 @@
 import React, { Fragment } from "react";
 import Planet from "./planet";
 
+async function getPlanets() {
+    let response = await fetch('http://localhost:3000/api/planets.json')
+    let data = await response.json()
+    return data;
+}
+
 
 class Planets extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            planets: [
-                {
-                title: "Mercúrio",
-                description: "lorem servado diretamente orbitando poior membro conhecido do cinturão de Kuiper,[11] uma  Sol. Originalmente classificado como um",
-                img_url: "http://www.ccvalg.pt/astronomia/sistema_solar/mercurio/mercurio_mariner_10.gif",
-                link: "https://mundoeducacao.uol.com.br/geografia/mercurio.htm"
-                },
-                
-                {
-                title: "Plutão",
-                description: "Plutão, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[11] uma região de corpos além da órbita de Netuno.",
-                img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
-                }
-
-            ]    
+            planets: []    
         }
+    }
+
+    componentDidMount() {
+        getPlanets().then(data => {
+            this.setState(state => ({
+                planets: data['planets']
+            }))
+        })
+
     }
 
     removeLast = () => {
