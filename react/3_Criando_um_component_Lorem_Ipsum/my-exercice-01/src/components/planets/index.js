086f5ce917c02470e9120a255ec1af5a7aa609a1
borgesds <borges.an@hotmail.com>
2022-06-13 17:24:01 -0300

    up React state

--- react/3_Criando_um_component_Lorem_Ipsum/my-exercice-01/src/components/planets/index.js
+++ react/3_Criando_um_component_Lorem_Ipsum/my-exercice-01/src/components/planets/index.js
@@ -1,23 +1,63 @@
 import React, { Fragment } from "react";
 import Planet from "./planet";
 
-const Planets = () => {
-    return (
-        <Fragment>
-            <h3>Planet List</h3>
-            <hr/>
-            <Planet title="Mercúrio" 
-                        description="Mercúrio é o menor[nota 1][nota 2] e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. Mercúrio completa três rotações em torno de seu eixo a cada duas órbitas. O periélio da órbita de Mercúrio apresenta uma precessão de 5 600 segundos de arco por século" 
-                        img_url="http://www.ccvalg.pt/astronomia/sistema_solar/mercurio/mercurio_mariner_10.gif"
-                        
-            />
 
-            <Planet title="Plutão" 
-                        description="Plutão, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[11] uma região de corpos além da órbita de Netuno." 
-                        img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
-            />
-        </Fragment>
-    )
+class Planets extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            planets: [
+                {
+                title: "Mercúrio",
+                description: "lorem servado diretamente orbitando poior membro conhecido do cinturão de Kuiper,[11] uma  Sol. Originalmente classificado como um",
+                img_url: "http://www.ccvalg.pt/astronomia/sistema_solar/mercurio/mercurio_mariner_10.gif",
+                link: "https://mundoeducacao.uol.com.br/geografia/mercurio.htm"
+                },
+                
+                {
+                title: "Plutão",
+                description: "Plutão, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[11] uma região de corpos além da órbita de Netuno.",
+                img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
+                }
+
+            ]    
+        }
+    }
+
+    removeLast = () => {
+        let new_planets =[...this.state.planets]
+        new_planets.pop()
+        this.setState(state => ({
+            planets: new_planets
+        }))
+    }
+
+    duplicateLastPlanet = () => {
+        let last_planet = this.state.planets[this.state.planets.length - 1]
+        this.setState(state => ({
+            planets: [...this.state.planets, last_planet]
+        }))
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <h3>Planet List</h3>
+                <button onClick={this.removeLast}>Remove Last!</button>
+                <button onClick={this.duplicateLastPlanet}>Duplicate Last!</button>
+                <hr />
+                {this.state.planets.map((planet) =>
+                    <Planet
+                        title={planet.title}
+                        description={planet.description}
+                        img_url={planet.img_url}
+                        link={planet.link}
+                    />
+                )}
+            </Fragment>
+        )
+    }
 }
+
 
 export default Planets;
--- react/3_Criando_um_component_Lorem_Ipsum/my-exercice-01/src/components/planets/planet/index.js
+++ react/3_Criando_um_component_Lorem_Ipsum/my-exercice-01/src/components/planets/planet/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import GrayImg from "../../shered/gray-image"
+import DescriptionWithLink from "../../shered/descriptin_whith_link";
 
 const Planet = (props) => {
+    let title;
+    if(props.title_with_underline) {
+        title = <h4><u>{props.title}</u></h4>
+    } else {
+        title = <h4>{props.title}</h4>
+    }
+
     return (
         <div>
-            <h4>{props.title}</h4>
-            <p>{props.description}</p>
-            <GrayImg img_url={props.img_url}/>
+            {title}
+            <DescriptionWithLink description={props.description} link={props.link}/>
+            <GrayImg img_url={props.img_url} gray={props.gray}/>
+            <hr/>
         </div>
     )
 }
