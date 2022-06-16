0f7b666cb59cfcb19ba63b561deee900c60f245c
borgesds <borges.an@hotmail.com>
2022-06-16 18:08:02 -0300

    up NodeJs

---
+++ nodejs/1_js_no_terminal/io.js
@@ -0,0 +1,10 @@
+const process = require('process')
+
+console.log('Digite seu nome: ')
+
+process.stdin.on('data', (keyboard) => {
+    process.stdout.write(`Texto do usuario: ${keyboard}`)
+    process.exit()
+})
+
+
