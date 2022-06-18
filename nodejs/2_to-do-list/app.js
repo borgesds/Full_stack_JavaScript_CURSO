40c04d9b5d63b90c68cd22efb83cf66988dc241d
borgesds <borges.an@hotmail.com>
2022-06-18 09:29:58 -0300

    up NodeJs Middlewares

--- nodejs/2_to-do-list/app.js
+++ nodejs/2_to-do-list/app.js
@@ -1,1 +1,32 @@
+//Import express
 const express = require('express')
+
+const app = express()
+
+
+//Verifica se tem algum json para deixar disponível
+app.use(express.json())
+
+//Middlewares - Logar informações
+const log = (req, res, next) => {
+    console.log(req.body)
+    console.log(`Data: ${Date.now()}`)
+    next()
+}
+
+app.use(log)
+
+//Localhost:3000
+app.get('/', (req, res) => {
+    res.send('<h1>Minha lista de tarefas</h1>')
+})
+
+//Localhost:3000/json
+app.get('/json', (req, res) => {
+    res.json({title: 'Tarefa x', done: true})
+})
+
+//Chamar o servidor
+app.listen(3000, () => {
+    console.log('Servidor Online')
+})
