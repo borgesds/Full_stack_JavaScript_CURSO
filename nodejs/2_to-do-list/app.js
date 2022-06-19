c4e43d097be496052466b4607644bb273dbf82d5
borgesds <borges.an@hotmail.com>
2022-06-19 08:14:44 -0300

    up NodeJs GET POST PUT DELETE

--- nodejs/2_to-do-list/app.js
+++ nodejs/2_to-do-list/app.js
@@ -1,30 +1,32 @@
-//Import express
+//Import
 const express = require('express')
+const checkListRouter = require('./src/routes/checklist')
+
 
 const app = express()
-
-
 //Verifica se tem algum json para deixar disponível
 app.use(express.json())
 
-//Middlewares - Logar informações
-const log = (req, res, next) => {
-    console.log(req.body)
-    console.log(`Data: ${Date.now()}`)
-    next()
-}
+app.use('/checklist', checkListRouter) //Todas as rotas que estirem dentro vão sem derivadas do checklist.js
 
-app.use(log)
+// //Middlewares - Logar informações
+// const log = (req, res, next) => {
+//     console.log(req.body)
+//     console.log(`Data: ${Date.now()}`)
+//     next()
+// }
 
-//Localhost:3000
-app.get('/', (req, res) => {
-    res.send('<h1>Minha lista de tarefas</h1>')
-})
+// app.use(log)
 
-//Localhost:3000/json
-app.get('/json', (req, res) => {
-    res.json({title: 'Tarefa x', done: true})
-})
+// //Localhost:3000
+// app.get('/', (req, res) => {
+//     res.send('<h1>Minha lista de tarefas</h1>')
+// })
+
+// //Localhost:3000/json
+// app.get('/json', (req, res) => {
+//     res.json({title: 'Tarefa x', done: true})
+// })
 
 //Chamar o servidor
 app.listen(3000, () => {
