433dce83ee5dc1c52821fd3333017899163d2ad7
Borges <borges.an@hotmai.com>
2022-07-07 21:21:23 -0300

    up sequelize

---
+++ nodejs_e_banco_de_dados/sequelize/controller/PlanetController.js
@@ -0,0 +1,41 @@
+// Aqui é onde vai ter os métodos(POST, GET, PUT, DELETE)
+const Planet = require("../models/planet")
+
+module.exports = {
+    async store(req, res) {
+        const {name, position} = req.body
+
+        const planet = await Planet.create({name, position})
+        return res.json(planet)
+    },
+
+    async index(req, res) {
+        const planet = await Planet.findAll()
+
+        return res.json(planet)
+    },
+
+    async put(req, res) {
+        const {name, size, position} = req.body
+        await Planet.update(
+            {name, size, position},
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        )
+        return res.send("Planet atualizado com sucesso")
+    },
+
+    async delete(req, res) {
+        await Planet.destroy(
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        )
+        return res.send("Planet DELETADO com sucesso")
+    }
+}
